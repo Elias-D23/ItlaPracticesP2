d87e3d77883c0bf3047c05302daeddd2980bc22b
Elias-D23 <Eliasdiazfeliz@gmail.com>
2024-10-26 21:33:10 -0400

    Tarea 3 y 4

---
+++ Crud/Crud.API/Controllers/PeopleController.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Core;
+using Crud.API.Dtos;
+using Crud.Domain.Entities;
+using Crud.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud.API.Controllers
+{
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly CrudDbContext _context;
+
+        public PeopleController(CrudDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetPeople")]
+        public async Task<ActionResult<List<Person>>> GetPeople()
+        {
+            return await _context.People.ToListAsync();
+        }
+
+        [HttpPost("AddPeople")]
+        public async Task<ActionResult<List<Person>>> AddPerson(Person person)
+        {
+           
+            //var personDb = new Person();
+
+            //personDb.Name = request.Name;
+            //personDb.LastName = request.LastName;
+            //personDb.Email = request.Email;
+            //personDb.Phone = request.Phone;
+
+            List<Person> people = new List<Person>();
+            people.Add(person);
+
+            _context.People.Add(person);
+            await _context.SaveChangesAsync();
+            return people;
+        }
+    }
+}
---
+++ Crud/Crud.Web/Controllers/PeopleController.cs
@@ -0,0 +1,67 @@
+﻿using Crud.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Crud.Domain.Entities;
+
+namespace Crud.Web.Controllers
+{
+
+    public class PeopleController : Controller
+    {
+
+        private readonly CrudDbContext _Dbcontext;
+
+        public PeopleController(CrudDbContext context)
+        {
+            _Dbcontext = context;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Person person)
+        {
+            await _Dbcontext.People.AddAsync(person);
+            await _Dbcontext.SaveChangesAsync();
+            return RedirectToAction(nameof(List));
+        }
+
+
+        [HttpGet]
+        public async  Task<IActionResult> List()
+        {
+            List<Person> listPersons = await _Dbcontext.People.ToListAsync();
+            return View(listPersons);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            Person person = await _Dbcontext.People.FirstAsync(e => e.Id == id);
+            return View(person);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Person person)
+        {
+            _Dbcontext.People.Update(person);
+            await _Dbcontext.SaveChangesAsync();
+            return RedirectToAction(nameof(List));
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Person person = await _Dbcontext.People.FirstAsync(e => e.Id == id);
+
+            _Dbcontext.People.Remove(person);
+            await _Dbcontext.SaveChangesAsync();
+            return RedirectToAction(nameof(List));
+        }
+    }
+}
