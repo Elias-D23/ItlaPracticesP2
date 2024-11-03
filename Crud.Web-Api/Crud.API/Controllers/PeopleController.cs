eb043dbb48ff0ec04f787b287171fdfa12829026
Elias-D23 <Eliasdiazfeliz@gmail.com>
2024-11-02 21:02:45 -0400

    Tarea 5

---
+++ Crud.Web-Api/Crud.API/Controllers/PeopleController.cs
@@ -0,0 +1,76 @@
+﻿using Azure.Core;
+using Crud.API.Dtos;
+using Crud.Domain.Entities;
+using Crud.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud.API.Controllers
+{
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        private readonly CrudDbContext _context;
+
+        public PeopleController(CrudDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetPerson")]
+        public async Task<ActionResult<List<Person>>> GetPerson()
+        {
+            return await _context.People.ToListAsync();
+        }
+
+        [HttpPost("AddPerson")]
+        public async Task<ActionResult<List<Person>>> AddPerson(NewPersonRequest request)
+        {
+
+            var personDb = new Person();
+
+            personDb.Name = request.Name;
+            personDb.LastName = request.LastName;
+            personDb.Email = request.Email;
+            personDb.Phone = request.Phone;
+
+            _context.People.Add(personDb);
+            await _context.SaveChangesAsync();
+            return Ok(personDb);
+        }
+
+        [HttpPut("UpdatePerson")]
+        public async Task<IActionResult> UpdatePerson(int id, [FromBody] NewPersonRequest request)
+        {
+            var personDb = await _context.People.FindAsync(id);
+            if (personDb == null)
+            {
+                return NotFound();
+            }
+
+            personDb.Name = request.Name;
+            personDb.LastName = request.LastName;
+            personDb.Email = request.Email;
+            personDb.Phone = request.Phone;
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("DeletePerson/{id}")]
+        public async Task<IActionResult> DeletePerson(int id)
+        {
+            var person = await _context.People.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _context.People.Remove(person);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ Crud.Web-Api/Crud.Domain/CrudDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Crud.Domain.Entities;
+
+
+namespace Crud.Web.Data
+{
+    public class CrudDbContext : DbContext
+    {
+        public CrudDbContext(DbContextOptions<CrudDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Person> People { get; set; }
+
+        //CREO QUE LO DEBO BORRAR :v
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Person>().HasIndex(p => p.Id).IsUnique();
+
+        }
+    }
+}
